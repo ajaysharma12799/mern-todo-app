dbe0f690dd053a3b8b65152f05e5216389a30f4c
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-01-12 15:56:25 +0530

    fixed db config

--- config/DatabaseConfig.js
+++ config/DatabaseConfig.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
-const DBConfig = mongoose.connect(process.env.DBURL, {
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useNewUrlParser: true
-})
+const ConnectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        console.log(`DataBase Connected : ${conn.connection.host}`);
+    }
+    catch(error) {
+        console.log(`DataBase Failed To Connect : ${error}`);
+        process.exit(1);
+    }
+};
 
-module.exports = DBConfig;
+ module.exports = ConnectDB;
