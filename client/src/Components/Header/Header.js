6d38910989395375c66c5c8c20c4c7a40e596101
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-01-18 17:40:08 +0530

    Added 'No Todo Found'

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1">MERN TODO APP</span>
+            <nav className="navbar navbar-light bg-primary">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0">MERN TODO APP</span>
                 </div>
             </nav>
         </div>
