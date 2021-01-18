92bffe4f5cc0adcb9918ca5304ce023d20ea320c
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-01-18 22:58:32 +0530

    added index.html serving

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ConnectDB = require("./config/DatabaseConfig");
 const cors = require("cors");
+const path = require('path');
 const TodoRoutes = require("./routes/TodoRoutes");
 
 const PORT = 3100 || process.env.PORT;
@@ -21,6 +22,15 @@ ConnectDB();
 // 3rd Party Dependency
 app.use(cors());
 app.use('/api/', TodoRoutes);
+
+// Server Static Assets if We are in Production
+if(process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.get('/', (req, res) => {
     res.json({
